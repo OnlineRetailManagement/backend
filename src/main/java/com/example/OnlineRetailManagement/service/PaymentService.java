4a9032ceec1274b16afce72a42052bd318dc8127
EdwinVincent28 <edwinvincent.evt@gmail.com>
2025-06-10 17:00:26 +0200

    error resolved

--- src/main/java/com/example/OnlineRetailManagement/service/PaymentService.java
+++ src/main/java/com/example/OnlineRetailManagement/service/PaymentService.java
@@ -3,9 +3,11 @@ package com.example.OnlineRetailManagement.service;
 import com.example.OnlineRetailManagement.entity.Payment;
 import com.example.OnlineRetailManagement.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PaymentService {
     @Autowired
     private PaymentRepository paymentRepository;

8ebdc3f56bbe1ea50816214ee9116531a66b60e5
EdwinVincent28 <edwinvincent.evt@gmail.com>
2025-07-27 13:01:02 +0200

    cors issue render

--- src/main/java/com/example/OnlineRetailManagement/config/WebConfig.java
+++ src/main/java/com/example/OnlineRetailManagement/config/WebConfig.java
@@ -14,8 +14,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
+                        .allowedOrigins("https://online-retail-management-frontend.onrender.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
